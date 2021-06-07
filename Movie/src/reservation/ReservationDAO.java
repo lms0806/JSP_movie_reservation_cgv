9ee3ad097caf98d1acb93197d70071f49f167d64
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-06-07 16:22:35 +0900

    Update ReservationDAO.java

--- Movie/src/reservation/ReservationDAO.java
+++ Movie/src/reservation/ReservationDAO.java
@@ -13,10 +13,10 @@ public class ReservationDAO {
 	
 	public ReservationDAO() {
 		try {
-			String dbURL = "jdbc:mysql://localhost:3306/movie?serverTimezone=UTC";
-			String dbID = "root";
-			String dbPassword = "3510";
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			String dbURL = "";
+			String dbID = "";
+			String dbPassword = "";
+			Class.forName("");
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
 		} catch(Exception e) {
 			e.printStackTrace();
